b89352b413097fe4485ca1a91552ff0be05b7142
untitledmemory <kendrix099@icloud.com>
2025-12-14 12:50:31 +0100

    Modify late game settings and respawn options
    Updated configuration settings for late game and respawns.

--- Erbium/Erbium/Public/Configuration.h
+++ Erbium/Erbium/Public/Configuration.h
@@ -2,20 +2,20 @@
 
 struct FConfiguration
 {
-    static inline auto Playlist = L"/Game/Athena/Playlists/Playlist_DefaultSolo.Playlist_DefaultSolo";
-    static inline auto MaxTickRate = 30;
-    static inline auto bLateGame = false;
-    static inline auto LateGameZone = 3; // starting zone
-    static inline auto bLateGameLongZone = false; // zone doesnt close for a long time
+    static inline auto Playlist = L"Playlist_ShowdownAlt_Solo";
+    static inline auto MaxTickRate = 30; // max storm dmg
+    static inline auto bLateGame = true;
+    static inline auto LateGameZone = 3; // lategame starting zone
+    static inline auto bLateGameLongZone = true; // zone doesnt close for a long time if u want infinite respawns lategame
     static inline auto bEnableCheats = true;
-    static inline auto SiphonAmount = 50; // set to 0 to disable
+    static inline auto SiphonAmount = 50; // choose ur siphon for kill right here! 0 is disable
     static inline auto bInfiniteMats = false;
     static inline auto bInfiniteAmmo = false;
-    static inline auto bForceRespawns = false; // build your client with this too!
-    static inline auto bJoinInProgress = false;
-    static inline auto bAutoRestart = false;
-    static inline auto bKeepInventory = false;
-    static inline auto Port = 7777;
+    static inline auto bForceRespawns = true; // build your client with this too!/this is for respawns
+    static inline auto bJoinInProgress = false; // tbh idk if it works never checked
+    static inline auto bAutoRestart = false; // restart backend when match finished
+    static inline auto bKeepInventory = false; // keep inventory
+    static inline auto Port = 7777; // keep this the same if ur using nexa or other backends!
     static inline constexpr auto bEnableIris = true; 
     static inline constexpr auto bGUI = true;
     static inline constexpr auto bCustomCrashReporter = true;
